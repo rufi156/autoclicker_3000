3f61f4d60adf47755e583c34af0ce7407558181a
Rafal Karpinski <rufi156@gmail.com>
2022-02-05 11:04:01 +0100

    change indent; add while true

--- bot.py
+++ bot.py
@@ -11,28 +11,31 @@ ImageGrab.grab = partial(ImageGrab.grab, all_screens=True)
 
 region_window = gw.getWindowsWithTitle('BlueStack')[0]
 REGION = (region_window.left, region_window.top, region_window.width, region_window.height)
+CONFIDENCE = 0.9
 
-achiev = None
-while achiev is None:
-    achiev = ag.locateCenterOnScreen('pic/achievements_on.png', region=REGION, confidence=0.9)
-    time.sleep(1)
-print("achev found")
+while True:
+    achiev = None
+    while achiev is None:
+        achiev = ag.locateCenterOnScreen('pic/achievements_on.png', region=REGION, confidence=CONFIDENCE)
+        time.sleep(1)
+    print('achiev found')
 
-exit = None
-while exit is None:
-    ag.click(achiev[0], achiev[1])
-    time.sleep(0.1)
-    exit = ag.locateCenterOnScreen('pic/exit_achiev.png', region=REGION, confidence=0.9)
-print('achiev entered')
+    exit = None
+    while exit is None:
+        ag.click(achiev[0], achiev[1])
+        time.sleep(0.1)
+        exit = ag.locateCenterOnScreen('pic/exit_achiev.png', region=REGION, confidence=CONFIDENCE)
+    print('achiev entered')
 
-collect = None
-while collect is None:
-    collect = ag.locateAllOnScreen('pic/achievement_collectable.png', region=REGION, confidence=0.9)
-print('collectibles found')
+    collect = None
+    while collect is None:
+        collect = ag.locateAllOnScreen('pic/achievement_collectable.png', region=REGION, confidence=CONFIDENCE)
 
-centers = list(map(lambda x: ag.center(x), collect))
-for c in centers:
-    ag.click(c[0], c[1])
-    time.sleep(0.05)
+    centers = list(map(lambda x: ag.center(x), collect))
+    for c in centers:
+        ag.click(c[0], c[1])
+        time.sleep(0.05)
+    print('achiev collected')
 
-ag.click(exit[0], exit[1])
+    ag.click(exit[0], exit[1])
+    print('exit')
