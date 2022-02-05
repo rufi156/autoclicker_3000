26b55c98e325ee3d96ab163325a756ce2d1e20db
Rafal Karpinski <rufi156@gmail.com>
2022-02-05 16:49:02 +0100

    add watch and skip ad +todos

--- bot.py
+++ bot.py
@@ -17,8 +17,7 @@ def collect_achiev():
     """
     checks whether achievements are available, if true then collects them and exits achievements window
     """
-    run = ag.locateOnScreen('pic/run_is_on.png', region=REGION, confidence=CONFIDENCE)
-    while run is not None:
+    while True:
         achiev = None
         while achiev is None:
             achiev = ag.locateCenterOnScreen('pic/achievements_on.png', region=REGION, confidence=CONFIDENCE)
@@ -35,8 +34,8 @@ def collect_achiev():
         collect = None
         while collect is None:
             collect = ag.locateAllOnScreen('pic/achievement_collectable.png', region=REGION, confidence=CONFIDENCE)
-
         centers = list(map(lambda x: ag.center(x), collect))
+
         for c in centers:
             ag.click(c[0], c[1])
             time.sleep(0.05)
@@ -55,34 +54,118 @@ def reset_run(level):
     map = None
     while map is None:
         ag.click(settings[0], settings[1])
-        time.sleep(0.1)
+        time.sleep(0.2)
         map = ag.locateCenterOnScreen('pic/map_select.png', region=REGION, confidence=CONFIDENCE)
     print('settings entered')
 
     mode = None
     while mode is None:
         ag.click(map[0], map[1])
-        time.sleep(0.1)
-        mode = ag.locateAllOnScreen('pic/normal.png', region=REGION, confidence=CONFIDENCE)
+        time.sleep(0.2)
+        mode = ag.locateCenterOnScreen('pic/'+level+'_normal.png', region=REGION, confidence=CONFIDENCE)
     print('mode entered')
-
-    centers = list(map(lambda x: ag.center(x), mode))
-    selected_mode = centers[level]
 
     confirm = None
     while confirm is None:
-        ag.click(selected_mode[0], selected_mode[1])
-        time.sleep(0.1)
+        ag.click(mode[0], mode[1]+25)
+        time.sleep(0.2)
         confirm = ag.locateCenterOnScreen('pic/confirm.png', region=REGION, confidence=CONFIDENCE)
     print('pick_monster entered')
 
-    in_game = None
-    while in_game is None:
-        ag.click(confirm[0], confirm[1])
-        time.sleep(0.1)
-        confirm = ag.locateCenterOnScreen('pic/run_is_on.png', region=REGION, confidence=CONFIDENCE)
+    x = confirm[0]
+    y = confirm[1]
+    while confirm is not None:
+        ag.click(x, y)
+        time.sleep(0.2)
+        confirm = ag.locateCenterOnScreen('pic/confirm.png', region=REGION, confidence=CONFIDENCE)
     print('run started')
 
-#reset_run(0)
+def enter_ad():
+    ad = None
+    stone = None
+    timer = 0
+    while timer<30 and (ad is None or (stone is None)): #and orb is None)):
+        ad = ag.locateCenterOnScreen('pic/ad.png', region=REGION, confidence=CONFIDENCE)
+        stone = ag.locateCenterOnScreen('pic/stone.png', region=REGION, confidence=CONFIDENCE)
+        #orb = ag.locateCenterOnScreen('pic/orb.png', region=REGION, confidence=CONFIDENCE)
+
+        time.sleep(1)
+        timer += 1
+    print('ad found')
+
+    while ad is not None and stone is not None:
+        ag.click(ad[0], ad[1])
+        time.sleep(1)
+        ad = ag.locateCenterOnScreen('pic/ad.png', region=REGION, confidence=CONFIDENCE)
+    print('ad entered')
+    if ad is not None:
+        return 1
+    else:
+        return 0
+
+def skip_ad():
+    arrow = None
+    arrow2 = None
+    arrow3 = None
+    arrow4 = None
+    exit = None
+    exit2 = None
+    exit3 = None
+    exit4 = None
+    timer = 0
+    while timer<5 and arrow is None and arrow2 is None and arrow3 is None and arrow4 is None:
+        arrow = ag.locateCenterOnScreen('pic/ad_arrow.png', grayscale=True, region=REGION, confidence=CONFIDENCE)
+        arrow2 = ag.locateCenterOnScreen('pic/ad_arrow2.png', grayscale=True, region=REGION, confidence=CONFIDENCE)
+        arrow3 = ag.locateCenterOnScreen('pic/ad_arrow3.png', grayscale=True, region=REGION, confidence=CONFIDENCE)
+        arrow4 = ag.locateCenterOnScreen('pic/ad_arrow4.png', grayscale=True, region=REGION, confidence=CONFIDENCE)
+        time.sleep(1)
+        timer += 1
+        print(timer)
+    if timer<5:
+        print('arrow found')
+        print([arrow, arrow2, arrow3,arrow4])
+        arrow = list(filter(None, [arrow,arrow2,arrow3,arrow4]))[0]
+
+        if arrow is not None:
+            ag.click(arrow[0], arrow[1])
+
+    while exit is None and exit2 is None and exit3 is None and exit4 is None:
+        exit = ag.locateCenterOnScreen('pic/ad_x.png', grayscale=True, region=REGION, confidence=CONFIDENCE)
+        exit2 = ag.locateCenterOnScreen('pic/ad_x2.png', grayscale=True, region=REGION, confidence=CONFIDENCE)
+        exit3 = ag.locateCenterOnScreen('pic/ad_x3.png', grayscale=True, region=REGION, confidence=CONFIDENCE)
+        exit4 = ag.locateCenterOnScreen('pic/ad_x4.png', grayscale=True, region=REGION, confidence=CONFIDENCE)
+        time.sleep(1)
+    print('x found')
+
+    print([exit, exit2, exit3, exit4])
+    exit = list(filter(None, [exit, exit2, exit3, exit4]))[0]
+    if exit is not None:
+        ag.click(exit[0], exit[1])
+
+    ad = None
+    while ad is None:
+        time.sleep(0.2)
+        ad = ag.locateCenterOnScreen('pic/ad_accept_reward.png', region=REGION, confidence=CONFIDENCE)
+    print('ad exited')
+
+    while ad is not None:
+        ag.click(ad[0], ad[1])
+        time.sleep(0.2)
+        ad = ag.locateCenterOnScreen('pic/ad_accept_reward.png', region=REGION, confidence=CONFIDENCE)
+    print('ad collected')
+
+#while True:
+#    if not enter_ad():
+#        reset_run('king')
 collect_achiev()
+"""
+skip_ad()
+reset_run('king')
+if not enter_ad():
+    reset_run('king')
+else:
+    time.sleep(40)
+    skip_ad()
+    rest_run('king')
 print('done')
+"""
